aaab230b4a9782f5987fe9269e371b0480611386
MrSullivan <>
2021-04-20 12:27:44 +0000

    while command set with input into variable and if statement leads to break in loop.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    while True:
+      print("Carry on? (n shows no)")
+      message = input()
+      if (message == "n"):
+        break
+
+
+
 
 if __name__ == '__main__':
     main()
